4e75bb3e827df1777bb0681fc2dd493c721ad6ed
y4smln <168206351+y4smln@users.noreply.github.com>
2024-05-28 20:07:58 -0300

    Update AtualizarAluno.js

--- screens/AtualizarAluno.js
+++ screens/AtualizarAluno.js
@@ -9,9 +9,9 @@ const CadAluno = () => {
   return (
     <View style={styles.cadaluno}>
       <SafeAreaView style={styles.banner}>
-        <Text style={styles.subtitle}>Atualizar Aluno</Text>
+        <Text style={styles.titulo}>Atualizar Aluno</Text>
         <Image
-           source={require('../assets/alunoImg.png')}
+           source={require('../assets/atualizarImg.png')}
           style={styles.image}
         />
       </SafeAreaView>
@@ -50,8 +50,8 @@ const CadAluno = () => {
         </View>
       </SafeAreaView>
 
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Continuar</Text>
+      <TouchableOpacity style={styles.btnCon}>
+        <Text style={styles.txtCon}>Continuar</Text>
       </TouchableOpacity>
     </View>
   );
@@ -68,13 +68,13 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   image: {
-    width: 250,
-    height: 250,
+    width: 350,
+    height: 350,
   },
-  subtitle: {
-    marginTop:20,
+  titulo: {
+    marginTop:30,
     fontSize: 30,
-    color: '#000',
+    color: '#FFA404',
     marginBottom: 20,
     textAlign: 'center',
     fontWeight: 'bold',
@@ -90,7 +90,7 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     marginBottom: 20,
     fontSize: 16,
-    borderColor: '#000',
+    borderColor: '#40A2E3',
     borderWidth: 1,
   },
   pickerContainer: {
@@ -99,14 +99,14 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     justifyContent: 'center',
     marginBottom: 20,
-    borderColor: '#000',
+    borderColor: '#40A2E3',
     borderWidth: 1,
   },
   picker: {
     height: 50,
     color: '#000',
   },
-  button: {
+  btnCon: {
     height: 50,
     backgroundColor: '#40A2E3',
     borderRadius: 25,
@@ -114,7 +114,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom:20
   },
-  buttonText: {
+  txtCon: {
     color: '#FFF',
     fontSize: 18,
     fontWeight: 'bold',
